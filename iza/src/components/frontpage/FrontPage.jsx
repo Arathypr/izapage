9ae3a4447b52900e3dc7e8ec239bfce1a051a4d4
Arathy <arathyp.radhakrishnan@gmail.com>
2023-11-04 17:16:49 +0530

    landingnotcomplete

--- iza/src/components/frontpage/FrontPage.jsx
+++ iza/src/components/frontpage/FrontPage.jsx
@@ -1,34 +1,144 @@
-import React from "react";
+// import React, { useState } from "react";
+// import "./FrontPage.css";
+
+// import image1 from "../../assets/pagetwobg_xl.png";
+// import image2 from "../../assets/pagefourbg_xl.png";
+// import image3 from "../../assets/pagethreebg_xl.png";
+
+// const FrontPage = () => {
+//   const [isClicked, setIsClicked] = useState(false);
+
+//   const handleClick = () => {
+//     setIsClicked(!isClicked);
+//   };
+
+// const FrontPage = () => {
+//   return (
+//     <figure id="blinds-window">
+// <img src={image1} alt="" className="first" />
+// <img src={image1} alt="" className="first" />
+// <img src={image1} alt="" className="first" />
+// <img src={image1} alt="" className="first" />
+// <img src={image1} alt="" className="first" />
+// <img src={image1} alt="" className="first" />
+// <img src={image1} alt="" className="first" />
+// <img src={image1} alt="" className="first" />
+// <img src={image1} alt="" className="first" />
+// <img src={image1} alt="" className="first" />
+// <img src={image2} alt="" className="second" />
+// <img src={image2} alt="" className="second" />
+// <img src={image2} alt="" className="second" />
+// <img src={image2} alt="" className="second" />
+// <img src={image2} alt="" className="second" />
+// <img src={image2} alt="" className="second" />
+// <img src={image2} alt="" className="second" />
+// <img src={image2} alt="" className="second" />
+// <img src={image2} alt="" className="second" />
+// <img src={image2} alt="" className="second" />
+//     </figure>
+//   );
+// };
+// export default FrontPage;
+// import React, { useState } from "react";
+// import "./FrontPage.css";
+
+// import image1 from "../../assets/pagethreebg_xl.png";
+// import image2 from "../../assets/pagefourbg_xl.png";
+// import image3 from "../../assets/pagethreebg_xl.png";
+
+// const FrontPage = () => {
+//   const [isClicked, setIsClicked] = useState(false);
+
+//   const handleClick = () => {
+//     setIsClicked(!isClicked);
+//   };
+
+//   return (
+//     <div>
+//       <figure id="blinds-window" className={isClicked ? "clicked" : ""}>
+//         <img src={image1} alt="" className="first" />
+//         <img src={image1} alt="" className="first" />
+//         <img src={image1} alt="" className="first" />
+//         <img src={image1} alt="" className="first" />
+//         <img src={image1} alt="" className="first" />
+//         <img src={image1} alt="" className="first" />
+//         <img src={image1} alt="" className="first" />
+//         <img src={image1} alt="" className="first" />
+//         <img src={image1} alt="" className="first" />
+//         <img src={image1} alt="" className="first" />
+
+//         <img src={image2} alt="" className="second" />
+//         <img src={image2} alt="" className="second" />
+//         <img src={image2} alt="" className="second" />
+//         <img src={image2} alt="" className="second" />
+//         <img src={image2} alt="" className="second" />
+//         <img src={image2} alt="" className="second" />
+//         <img src={image2} alt="" className="second" />
+//         <img src={image2} alt="" className="second" />
+//         <img src={image2} alt="" className="second" />
+//         <img src={image2} alt="" className="second" />
+//       </figure>
+//       <div
+//         style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+//       >
+//         <button onClick={handleClick}>Toggle Animation</button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FrontPage;
+import React, { useState } from "react";
 import "./FrontPage.css";
 
-import image1 from "../../assets/pagetwobg_xl.png";
+import image1 from "../../assets/pagethreebg_xl.png";
 import image2 from "../../assets/pagefourbg_xl.png";
-import image3 from "../../assets/pagethreebg_xl.png";
 
 const FrontPage = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
-    <figure id="blinds-window">
-      <img src={image1} alt="" className="first" />
-      <img src={image1} alt="" className="first" />
-      <img src={image1} alt="" className="first" />
-      <img src={image1} alt="" className="first" />
-      <img src={image1} alt="" className="first" />
-      <img src={image1} alt="" className="first" />
-      <img src={image1} alt="" className="first" />
-      <img src={image1} alt="" className="first" />
-      <img src={image1} alt="" className="first" />
-      <img src={image1} alt="" className="first" />
-      <img src={image2} alt="" className="second" />
-      <img src={image2} alt="" className="second" />
-      <img src={image2} alt="" className="second" />
-      <img src={image2} alt="" className="second" />
-      <img src={image2} alt="" className="second" />
-      <img src={image2} alt="" className="second" />
-      <img src={image2} alt="" className="second" />
-      <img src={image2} alt="" className="second" />
-      <img src={image2} alt="" className="second" />
-      <img src={image2} alt="" className="second" />
-    </figure>
+    <div>
+      <div className="container">
+        <div className={`blinds-window ${isClicked ? "clicked" : ""}`}>
+          <img src={image1} alt="" className="first" />
+          <img src={image1} alt="" className="first" />
+          <img src={image1} alt="" className="first" />
+          <img src={image1} alt="" className="first" />
+          <img src={image1} alt="" className="first" />
+          <img src={image1} alt="" className="first" />
+          <img src={image1} alt="" className="first" />
+          <img src={image1} alt="" className="first" />
+          <img src={image1} alt="" className="first" />
+          <img src={image1} alt="" className="first" />
+        </div>
+      </div>
+
+      <div className="container">
+        <div className={`blinds-window ${isClicked ? "clicked" : ""}`}>
+          <img src={image2} alt="" className="second" />
+          <img src={image2} alt="" className="second" />
+          <img src={image2} alt="" className="second" />
+          <img src={image2} alt="" className="second" />
+          <img src={image2} alt="" className="second" />
+          <img src={image2} alt="" className="second" />
+          <img src={image2} alt="" className="second" />
+          <img src={image2} alt="" className="second" />
+          <img src={image2} alt="" className="second" />
+          <img src={image2} alt="" className="second" />
+        </div>
+      </div>
+
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+      >
+        <button onClick={handleClick}>Toggle Animation</button>
+      </div>
+    </div>
   );
 };
 
